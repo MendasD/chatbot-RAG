33e2ffcb69c87476bd2c9ef37e8a7c994b40d516
MendasD <168176998+MendasD@users.noreply.github.com>
2026-01-26 23:19:15 +0000

    update clean dev

---
+++ noxa_app/base/base/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Topic)
+admin.site.register(Tag)
+admin.site.register(Publication)
+admin.site.register(Collection)
+admin.site.register(CollectionPublication)
+admin.site.register(Message)
+admin.site.register(Notification)
+admin.site.register(Discussion)
+admin.site.register(SearchHistory)
+admin.site.register(SearchSuggestion)
+admin.site.register(Specialization)
+admin.site.register(Classes)
+admin.site.register(Courses)
+admin.site.register(Subjects)
