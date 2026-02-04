d3d305dea7b7ea2172086d2e14dbb98f1166b56f
Abdou Ba <ba2664890@gmail.com>
2026-02-04 00:46:37 +0000

    ajout de file

--- noxa_app/base/Static/js/chat.js
+++ noxa_app/base/Static/js/chat.js
@@ -164,7 +164,7 @@ class NoxaChat {
         container.style.gap = '10px';
 
         // Show upload options option whenever there are files
-        if (uploadOptions) uploadOptions.style.display = 'flex';
+        if (uploadOptions) uploadOptions.style.display = 'block';
 
         container.innerHTML = this.selectedFiles.map((file, index) => `
             <div class="file-preview">
