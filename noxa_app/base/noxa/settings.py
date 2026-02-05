071ed50de16a9f62f0838bf7893701e117fff4c7
Abdou Ba <ba2664890@gmail.com>
2026-02-05 22:38:59 +0000

    feat: add cloudinary storage

--- noxa_app/base/noxa/settings.py
+++ noxa_app/base/noxa/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-8#-vp#m#wl*7@5)v6$x9hy-_bpg80axt1p$(hmc5x__nhvvnx5'
 
-# SECURITY WARNING: don't run with debug turned on in production!.
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app']
 
